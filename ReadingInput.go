3b4e21e6d48dd6b0b57e26c862d1c33409f8a7c3
Mohd Aiman Jaffer <aiman.jaffer@gmail.com>
2020-07-20 11:43:07 +0530

    Reading files using os.Open() and scanner from bufio

--- ReadingInput.go
+++ ReadingInput.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	/*scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		fmt.Println("you typed " + scanner.Text())
 		if scanner.Text() == "/quit" {
@@ -17,5 +17,18 @@ func main() {
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
+	}*/
+	file, err := os.Open("HelloWorld.go")
+	if err != nil {
+		fmt.Println(err)
 	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
 }
