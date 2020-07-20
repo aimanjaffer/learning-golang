0327514bb0f8af0ccba32bb4b5607925abfc2b87
Mohd Aiman Jaffer <aiman.jaffer@gmail.com>
2020-07-20 11:22:30 +0530

    Scanf reating quoted string

--- ReadingInput.go
+++ ReadingInput.go
@@ -7,6 +7,6 @@ import (
 func main() {
 	var name string
 	fmt.Println("What is your name")
-	inputs, _ := fmt.Scanf("%s", &name)
+	inputs, _ := fmt.Scanf("%q", &name)
 	fmt.Printf("Hello %s! nice to meet you. %d\n", name, inputs)
 }
