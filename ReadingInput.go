bac462e04660e7d2a2211e4c842cf9f2a7faeb89
Mohd Aiman Jaffer <aiman.jaffer@gmail.com>
2020-07-20 11:33:00 +0530

    using os.Exit() with code 3 to quit

--- ReadingInput.go
+++ ReadingInput.go
@@ -10,6 +10,10 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		fmt.Println("you typed " + scanner.Text())
+		if scanner.Text() == "/quit" {
+			fmt.Println("See ya later")
+			os.Exit(3)
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
